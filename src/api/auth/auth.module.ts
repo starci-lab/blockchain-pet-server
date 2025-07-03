392db22830dd7d683f08792d8821cea4b5a38804
Huỳnh Nguyên <140372018+nguyenhcp2004@users.noreply.github.com>
2025-07-03 22:02:38 +0700

    fix: add secret

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -4,11 +4,21 @@ import { AuthController } from './auth.controller';
 import { User, UserSchema } from 'src/api/user/schemas/user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    JwtModule.register({}),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('auth.accessSecret'),
+        signOptions: {
+          expiresIn: configService.get<string>('auth.accessExpires'),
+        },
+      }),
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
